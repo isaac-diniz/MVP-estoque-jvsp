85729b2d3c8cbbecbd8c557e75e5c011c8839427
Isaac Diniz <dinizisaac266@gmail.com>
2023-09-11 21:51:56 -0300

    Entities

---
+++ src/main/java/com/isaacdiniz/mvpestoque/MvpestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.isaacdiniz.mvpestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpestoqueApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/isaacdiniz/mvpestoque/entities/Department.java
@@ -0,0 +1,5 @@
+package com.isaacdiniz.mvpestoque.entities;
+
+public class Department {
+
+}
